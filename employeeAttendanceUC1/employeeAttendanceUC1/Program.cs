dbef9711eef7d88c8587eb9cd5a517c346a1eae7
Princekotadiya007 <121780771+Princekotadiya007@users.noreply.github.com>
2023-01-05 20:57:31 +0530

    Add files via upload

---
+++ employeeAttendanceUC1/employeeAttendanceUC1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employeeAttendanceUC1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //constants
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            //compution 
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
+
