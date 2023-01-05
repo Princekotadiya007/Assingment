1d3f90c3825b7be9bcdff9e7e7a19b0d154612bb
Princekotadiya007 <121780771+Princekotadiya007@users.noreply.github.com>
2023-01-05 21:37:10 +0530

    Add files via upload

---
+++ employeePartTimeUC3/employeePartTimeUC3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employeePartTimeUC3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //contants
+            int PART_TIME = 1;
+            int FULL_TIME = 2;
+            int EMP_RATE_HOUR = 20;
+            //variables
+            int empHrs = 0;
+            int empWage = 0;
+            //computation
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+
+            if(empCheck == PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_HOUR;
+
+            Console.WriteLine("emp wage" + empWage);
+        }
+    }
+}
